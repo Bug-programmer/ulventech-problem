e72a43d37553cf1161c1ec56ab92fea082331f72
Bug-programmer <connectdeen@gmail.com>
2021-11-09 00:04:14 +0530

    chore(main): add basic logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Works");
+    public static void main(String[] args) throws IOException {
+        byte[] strings = new byte[100];
+        Arrays.fill(strings, (byte) 'a');
+        int x = 10;
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < 100; j++) {
+                strings[j]++;
+                if (strings[j] <= 'z') {
+                    break;
+                } else {
+                    strings[j] = 'a';
+                }
+            }
+        }
     }
 }
