a11829b19ed500fae02c1dfd51dc96cd86d6bdf6
Bug-programmer <connectdeen@gmail.com>
2021-11-09 00:56:41 +0530

    refactor(main): reduce time complexity

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,27 +8,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         byte[] strings = new byte[100];
-        int x = 10;
+        int x = 230;
         String fileName = x + " lines with 100 unique characters";
         Arrays.fill(strings, (byte) 'a');
-
-        byte[] crlf = new byte[] {'\r', '\n'};
-
         FileOutputStream fileOutputStream = new FileOutputStream(fileName);
 
         for (int i = 0; i < x; i++) {
             fileOutputStream.write(strings);
-            fileOutputStream.write(crlf);
-            fileOutputStream.flush();
-
-            for (int j = 0; j < 100; j++) {
-                strings[j]++;
-                if (strings[j] <= 'z') {
-                    break;
-                } else {
-                    strings[j] = 'a';
-                }
-            }
+            fileOutputStream.write(10);
+            strings[i % 100] = ++strings[i % 100];
         }
 
         fileOutputStream.close();
