6c72b9bfca3d9adb919a800975b832d64394d478
Bug-programmer <connectdeen@gmail.com>
2021-11-09 01:20:59 +0530

    refactor(printer): update logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,13 @@
 package com.company;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        byte[] strings = new byte[100];
-        int x = 230;
-        String fileName = x + " lines with 100 unique characters";
-        Arrays.fill(strings, (byte) 'a');
-        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        short x =  230;
+        Printer printer = new Printer(x);
+       printer.print();
 
-        for (int i = 0; i < x; i++) {
-            fileOutputStream.write(strings);
-            fileOutputStream.write(10);
-            strings[i % 100] = ++strings[i % 100];
-        }
-
-        fileOutputStream.close();
     }
 }
--- src/com/company/Printer.java
+++ src/com/company/Printer.java
@@ -1,4 +1,35 @@
 package com.company;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+
 public class Printer {
+  private final byte[] randomCharacters;
+   private final String fileName;
+   private final short lineCount;
+    public Printer(short lineCount) {
+        this.lineCount = lineCount;
+        this.randomCharacters = new byte[100];
+        this.fileName =  lineCount + " lines with 100 unique characters";
+        Arrays.fill(this.randomCharacters, (byte) 'a');
+    }
+    private byte updateRandomCharacters(byte randomCharacters){
+        return ++randomCharacters;
+    }
+    void print() throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+
+        for (int i = 0; i <lineCount; i++) {
+            fileOutputStream.write(randomCharacters);
+            fileOutputStream.write(10);
+            randomCharacters[i % 100] = this.updateRandomCharacters(this.randomCharacters[i % 100]);
+        }
+
+        fileOutputStream.close();
+    }
+
+
 }
